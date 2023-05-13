d4284842ca81f5be8111ff6866e8e3102d96829d
Joy <mmehedihasanjoyv@gmail.com>
2023-05-14 04:24:00 +0600

    created the messeges sender

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -5,7 +5,8 @@ import { MdCall } from 'react-icons/md';
 
 const Messages = () => {
     return (
-        <div className='border-r-2 h-screen'>
+        <div>
+              <div className='border-r-2 h-screen'>
             {/* message header */}
            <div className='flex  px-3 py-1 bg-gray-100 parent-container '>
                 <img className='w-16 h-16 rounded-full ' src={women} alt="" />  
@@ -54,7 +55,26 @@ const Messages = () => {
                     
                 </div>
        </div>
+             
         </div>
+              
+<form className='border'>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+      
+        <div class="relative w-full">
+            <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="" required/>
+            <button type="submit" class="absolute top-0 right-0 bottom-0 p-2.5 text-sm font-medium text-white bg-blue-700  border hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+            Send
+            </button>
+        </div>
+    </div>
+</form>
+
+      </div>
     );
 };
 
